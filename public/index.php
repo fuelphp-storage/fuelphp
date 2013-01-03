aed9cedc30186cd5fc9cda37a6845daf447d68c8
WanWizard <wanwizard@wanwizard.eu>
2013-01-03 21:47:14 +0100

    simplified the index.php

--- public/index.php
+++ public/index.php
@@ -5,19 +5,13 @@
 require '../vendor/autoload.php';
 
 /**
- * And kickstart the framework!
+ * Initialize the environment and start the application!
  */
-
-// create an instance of the DiC
-$dic = new FuelPHP\DependencyInjection\Container();
-
-// register the Environment class to the DiC
-$dic->register('env', 'FuelPHP\Foundation\Environment', function($entry) {
-	$entry->preferSingleton();
-});
-
-// create the environment instance, configure it, and start the application...
-$dic->resolve('env', null, $dic)
-	->setConfig(array())
-	->setApp('../application/demoapp')
-	->execute();
+\FuelPHP\Foundation\Environment::singleton()
+	->init(
+		array(
+			'path' => '../packages',
+			'application' => 'demo-package'
+		)
+	)
+	->start();

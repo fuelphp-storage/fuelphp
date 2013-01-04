b60cea948faa95cd67d72d71aa267cde2cfa46d3
WanWizard <wanwizard@wanwizard.eu>
2013-01-04 17:36:02 +0100

    completed the initial index.php code

--- public/index.php
+++ public/index.php
@@ -1,17 +1,43 @@
 <?php
 /**
- * Include the Composer autoloader.
+ * Include the Composer autoloader
  */
 require '../vendor/autoload.php';
 
 /**
- * Initialize the environment and start the application!
+ * Load and initialize the application environment...
  */
-\FuelPHP\Foundation\Environment::singleton()
-	->init(
-		array(
-			'path' => '../packages',
-			'application' => 'demo-package'
-		)
+$env = FuelPHP::resolve('Environment')->init(
+	array(
+		'name'        => isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : 'development',
+		'application' => 'demo-package',
+		'path'        => '../packages',
 	)
-	->start();
+);
+
+/**
+ * and get us some response
+ */
+
+$response = $env->loadApplication()
+	->request($env->input->getPathInfo())
+	->execute()
+	->getResponse()
+	->sendHeaders()
+	->getContent();
+
+/**
+ * Compile profiling data
+ */
+$execTime = round($env->profiler->getTimeElapsed(), 5);
+$memUsage = round($env->profiler->getMemUsage() / 1000000, 4);
+$memPeakUsage = round($env->profiler->getMemUsage(true) / 1000000, 4);
+
+/**
+ * Output the response body and replace the profiling values
+ */
+echo str_replace(
+	array('{exec_time}', '{mem_usage}', '{mem_peak_usage}'),
+	array($execTime,     $memUsage,     $memPeakUsage),
+	$response
+);
